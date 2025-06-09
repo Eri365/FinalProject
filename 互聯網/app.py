0a65b053dbccc919674168c418c6980783787695
Eri365 <101698682+Eri365@users.noreply.github.com>
2025-06-09 23:23:42 +0800

    Update app.py

--- 互聯網/app.py
+++ 互聯網/app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template, jsonify, request
 from model.apnea_model import predict_apnea
+import requests
+import threading
+import time
 
 app = Flask(__name__)
 
-current_heart_rate = 75
-apnea_status = "未知"
+PULSOID_TOKEN = "98a0b9a9-0b8c-41de-acfa-51d9b3650b64"
+PULSOID_URL = "https://dev.pulsoid.net/api/v1/data/heart_rate/latest"
+HEADERS = {
+    "Authorization": f"Bearer {PULSOID_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+current_heart_rate = "--"
+apnea_status = '未知'
+
+def fetch_heart_rate_loop():
+    global current_heart_rate
+    while True:
+        try:
+            res = requests.get(PULSOID_URL, headers=HEADERS)
+            if res.status_code == 200:
+                data = res.json()
+                current_heart_rate = data["data"]["heart_rate"]
+            else:
+                current_heart_rate = "❌ 無法取得"
+        except Exception as e:
+            current_heart_rate = f"錯誤: {e}"
+        time.sleep(0.1)
+
+threading.Thread(target=fetch_heart_rate_loop, daemon=True).start()
 
 @app.route("/")
 def index():
@@ -13,11 +39,7 @@ def index():
 @app.route("/update_heart_rate", methods=["POST"])
 def update_heart_rate():
     global current_heart_rate, apnea_status
-    # 由以下這部分接收資料
-    data = request.json
-    current_heart_rate = data.get("heart_rate", 75) # 測試用
     apnea_status = predict_apnea(current_heart_rate)
-    # ###
     return jsonify({"status": "success"})
 
 @app.route("/get_status")
